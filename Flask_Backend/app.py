d97ce51641472f9108d7438b45e2c992ca62c5fc
Miro <Syktkot@gmail.com>
2025-03-14 00:10:10 +0100

    Fix: percorso template_folder per Render

--- Flask_Backend/app.py
+++ Flask_Backend/app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 app = Flask(__name__, template_folder=os.path.join(os.getcwd(), "Flask_Backend", "templates"))
 
-
 # Funzione per connettersi al database
 def connessione_db():
     return sqlite3.connect("clienti.db")
